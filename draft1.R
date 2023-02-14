4f378f432c706d85b8b0918c5722171786db3c99
Max Trinh <maxtnyc10@gmail.com>
2023-02-13 19:03:56 -0500

    Using a for loop to calculate the expected win value out of a dollar to confirm with the manufacturer's claim.

--- draft1.R
+++ draft1.R
@@ -9,10 +9,69 @@ payoffs <- c('DD' = 100, '7' = 80, 'BBB' = 40, 'BB' =25,
 prob = c(0.03, 0.03, 0.06, 0.1, 0.25, 0.01, 0.52)
 prob_name <- setNames(prob, wheel)
 
-# Create a matrix of column 3 and n rows of the wheel object
+# Create a matrix of column 3 of the wheel object
 get_symbols <- function(){
-    sample(x = wheel, size = 3 * n, replace = TRUE,
+    sample(x = wheel, size = 3, replace = TRUE,
            prob = prob)
 }
 
+score <- function(symbols){
+    # Diamonds ('DD') is a wild card
+    
+    cherries <- sum(symbols == 'C')
+    diamonds <- sum(symbols == 'DD')
+    
+    # This creates a vector of symbols not equal to DD
+    slot <- symbols[symbols != 'DD']
+    same <- length(unique(slot)) == 1
+    bars <- slot %in% c('BBB', 'BB', 'B')
+    
+    if (diamonds == 3) {
+        prize = 100
+    } else if (same){
+        prize <- unname(payoffs[slot[1]])   
+    # Any combination of bars is 5
+    } else if (all(bars)) {
+        prize <- 5
+    }
+    # One C is 2, Two C is 5
+    else if (cherries > 0) {
+        prize <- c(0, 2, 5)[cherries + diamonds + 1]
+    }
+    else {
+        prize <- 0
+    }
+    prize * 2^diamonds
+    
+}
 
+play <- function(){
+    symbols <- get_symbols()
+    score(symbols)
+}
+
+winnings <- vector(length = 1000000)
+x <- vector(length = 5)
+
+# Takes the average of five trials for more accuracy
+for (i in 1:5){
+    for (j in 1:length(winnings)) {
+        winnings[j] = play()
+    }
+    x[i] = mean(winnings)
+}
+
+
+mean(winnings) # Expected total win is 93.7 cents out of a dollar
+
+long <- function(){
+    for (j in 1:length(winnings)) {
+        winnings[j] = play()
+    }
+}
+
+system.time(long())
+#  user    system    elapsed
+# 26.091   0.143      26.285 
+# First two columns are how many seconds elapsed the user and system execute the process of the call
+# Last column is how long R ran the expression
